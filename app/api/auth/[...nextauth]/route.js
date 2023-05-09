f5b5ca4d869e0a5520097d8310a3de72b1e97e46
Max Lerman <mjl82601@gmail.com>
2023-05-09 16:26:39 +0100

    added user sign in/up checks

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -11,15 +12,31 @@ const handler = NextAuth({
     })
   ],
   async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email
+    });
 
+    session.user.id = sessionUser._id.toString();
+    
+    return session;
   },
   async signIn({ profile }) {
     try {
       await connectToDB();
 
       // check if user already exists
+      const userExists = await User.findOne({
+        email: profile.email
+      });
 
       // if not, create a new user and save to database
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture
+        });
+      }
 
       return true;
     }
