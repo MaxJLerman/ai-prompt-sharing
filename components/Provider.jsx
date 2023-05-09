5a9c431c6b68567623659a272a799e714612324f
Max Lerman <mjl82601@gmail.com>
2023-05-09 15:28:56 +0100

    updated provider file

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,6 +1,12 @@
-const Provider = () => {
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+const Provider = ({ children, currentSession }) => {
   return (
-    <div>Provider</div>
+    <SessionProvider session={currentSession}>
+      {children}
+    </SessionProvider>
   );
 };
 
