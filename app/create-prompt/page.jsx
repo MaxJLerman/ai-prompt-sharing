64b0f9a52e0758f39d679a622627d2edb8882beb
Max Lerman <mjl82601@gmail.com>
2023-05-11 15:02:47 +0100

    created post method API

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,6 +7,8 @@ import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
@@ -14,7 +16,33 @@ const CreatePrompt = () => {
   });
 
   const handleCreatePrompt = async (event) => {
+    event.preventDefault();
 
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    }
+
+    catch (error) {
+      console.log(error);
+    }
+
+    // clause that will be executed whether try succeedes or catch catches an error
+    finally {
+      setSubmitting(false);
+    }
   };
   
   return (
