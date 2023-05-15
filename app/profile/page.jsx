3151bf5bc6354aece7ac2ae63d439a1e0aded096
Max Lerman <mjl82601@gmail.com>
2023-05-15 12:49:55 +0100

    added edit and delete post functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
   const [myPosts, setMyPosts] = useState([]);
   
@@ -23,12 +24,28 @@ const MyProfile = () => {
     }
   }, []);
 
-  const handleEdit = () => {
-    
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async () => {
-    
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE"
+        });
+
+        // filters out the post to be deleted by comparing each one and letting through failed matches to be set in the useState
+        const filteredPosts = myPosts.filter((p) => p._id !== post._id);
+        setMyPosts(filteredPosts);
+      }
+
+      catch (error) {
+        console.log(error);
+      }
+    }
   };
   
   return (
